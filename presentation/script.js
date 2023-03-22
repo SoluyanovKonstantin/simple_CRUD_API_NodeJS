2b012265780f2202b10e2bd5bffbff2fb7adf14f
Konstantin Soluianov <sol.kot15@gmail.com>
2023-03-22 14:39:57 +0400

    complete presentation

--- presentation/script.js
+++ presentation/script.js
@@ -1,10 +1,108 @@
-console.log('hello')
-$.ajax({
-    url: "http://localhost:3000/api/getWeather",
-    data: {
-      zipcode: 97201
-    },
+const url = 'http://localhost:3000/api/'
+let users;
+
+$('#getUsers').on('click', () => {
+  $.ajax({
+    type: 'GET',
+    url: `${url}users`,
+    dataType: 'JSON',
+    success: function( result ) {
+      users = result;
+      console.log(result)
+    }
+  })
+})
+
+$('#getUser').on('submit', function (ev) {
+  ev.preventDefault();
+  let data = {};
+  $(this).serializeArray().map(value => {
+    data[value.name] = value.value;
+  })
+
+  $.ajax({
+    type: 'GET',
+    url: `${url}users/${data.id}`,
+    dataType: 'JSON',
     success: function( result ) {
       console.log(result)
     }
+  })
 })
+
+$('#createUser').on('submit', function (ev) {
+  ev.preventDefault();
+  let data = {};
+  $(this).serializeArray().map(value => {
+    data[value.name] = value.value;
+  })
+
+  data['hobbies'] = [];
+
+  $('.hobbies').each((index, element) => {
+    if(element.value)
+      data['hobbies'].push(element.value);
+  });
+
+  $.ajax({
+    type: 'POST',
+    url: `${url}users`,
+
+    data: JSON.stringify(data),
+    contentType:"application/json; charset=utf-8",
+    success: function( result ) {
+      console.log(result)
+    }
+  })
+})
+
+$('#deleteUser').on('submit', function (ev) {
+  ev.preventDefault();
+  let data = {};
+  $(this).serializeArray().map(value => {
+    data[value.name] = value.value;
+  })
+
+  $.ajax({
+    type: 'DELETE',
+    url: `${url}users/${data.id}`,
+
+    success: function( result ) {
+      console.log(result)
+    }
+  })
+})
+
+$('#updateUser').on('submit', function (ev) {
+  ev.preventDefault();
+  let data = {};
+  $(this).serializeArray().map(value => {
+    data[value.name] = value.value;
+  })
+
+
+  $('.hobbies_edit').each((index, element) => {
+    if(element.value && data['hobbies'].length)
+      data['hobbies'].push(element.value);
+    else if(element.value)
+      data['hobbies'] = [element.value];
+  });
+
+  const id = data.id;
+  data.id = undefined;
+
+  $.ajax({
+    type: 'PUT',
+    url: `${url}users/${id}`,
+    contentType:"application/json; charset=utf-8",
+
+    data: JSON.stringify(data),
+    success: function( result ) {
+      console.log(result)
+    }
+  })
+})
+
+function addHobbie(id = 'hobbies') {
+  $('#' + id).append(`<input type="string" class="${id}" name="hobbies" placeholder="Хобби">`)
+}
